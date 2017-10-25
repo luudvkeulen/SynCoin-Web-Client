7f0e1dd3974beca0b144ebf3f874caed4b58c53a
Luud van Keulen <luudvkeulen@gmail.com>
2017-10-25 13:32:11 +0200

    Routing added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import {ClarityModule} from 'clarity-angular';
-import {RouterModule} from "@angular/router";
+import {AppRoutingModule} from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     ClarityModule,
-    RouterModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
