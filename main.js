12c166c458181fd3335303968b619641b954a4b6
Luud van Keulen <luudvkeulen@gmail.com>
2017-11-01 13:02:51 +0100

    Electron added.

---
+++ main.js
@@ -0,0 +1,27 @@
+const {app, BrowserWindow} = require('electron');
+
+let win = null;
+
+app.on('ready', function () {
+  win = new BrowserWindow({width: 1000, height: 600});
+
+  win.loadURL("http://localhost:4200");
+
+  win.webContents.openDevTools();
+
+  win.on('closed', function () {
+    win = null;
+  });
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
