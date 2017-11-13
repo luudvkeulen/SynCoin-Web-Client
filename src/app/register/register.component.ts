072f5712f99ccd0beb852f7edd8307b4839f3416
Luud van Keulen <luudvkeulen@gmail.com>
2017-11-13 14:57:09 +0100

    Added register page layout.

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class RegisterComponent implements OnInit {
 
