a26bfc4493c0ea7a94bfd4e5514f0cea50a73662
Nino Vrijman <nino-vrijman@users.noreply.github.com>
2017-11-21 15:00:18 +0100

    Finished login
    Shows feedback when login failed, when login is successfull a authentication token is saved in localStorage and the user is redirected to the wallet/balance component.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,53 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [AccountService]
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  fb: FormBuilder;
 
-  constructor() {
+  loginFailed: boolean;
+
+  constructor(
+    fb: FormBuilder, 
+    private accountService: AccountService,
+    private router: Router
+  ) {
+    this.fb = fb;
+    this.createForm();
+    this.loginFailed = false;
   }
 
   ngOnInit() {
+
   }
 
+  createForm() {
+    this.loginForm = this.fb.group({
+      'email': ['', [Validators.required, Validators.email]],
+      'password': ['', [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    if (!this.loginForm.valid) {
+      const controls = this.loginForm.controls;
+      Object.values(controls).forEach(control => control.markAsTouched());
+    } else {
+      this.accountService.login(this.loginForm.controls['email'].value, this.loginForm.controls['password'].value)
+        .subscribe(res => {
+          this.loginFailed = false;
+          this.router.navigateByUrl('/wallet/balance');
+        }, error => {
+          this.loginFailed = true;
+        });
+    }
+  }
 }
