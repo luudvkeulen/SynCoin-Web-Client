b6b494222d8a21e460309e29549028f2cac2a084
Nino Vrijman <nino-vrijman@users.noreply.github.com>
2017-11-28 15:31:11 +0100

    Orders can be manually cancelled or are automatically cancelled after a certain time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import {ClarityModule} from 'clarity-angular';
-import {AppRoutingModule} from './app-routing.module';
+import { ClarityModule } from 'clarity-angular';
+import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { ShopComponent } from './shop/shop.component';
 import { WalletComponent } from './wallet/wallet.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ShopProductsComponent } from './shop-products/shop-products.component';
-import {ShopOrdersComponent} from './shop-orders/shop-orders.component';
-import {WalletBalanceComponent} from './wallet-balance/wallet-balance.component';
-import {WalletSendComponent} from './wallet-send/wallet-send.component';
-import {WalletReceiveComponent} from './wallet-receive/wallet-receive.component';
-import {WalletTransactionsComponent} from './wallet-transactions/wallet-transactions.component';
-import {LoginComponent} from './login/login.component';
-import {RegisterComponent} from './register/register.component';
-import {QRCodeModule} from 'angular2-qrcode';
-import {QrScannerModule} from 'angular2-qrscanner/dist';
-import {TransactionComponent} from './transaction/transaction.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
+import { ShopOrdersComponent } from './shop-orders/shop-orders.component';
+import { WalletBalanceComponent } from './wallet-balance/wallet-balance.component';
+import { WalletSendComponent } from './wallet-send/wallet-send.component';
+import { WalletReceiveComponent } from './wallet-receive/wallet-receive.component';
+import { WalletTransactionsComponent } from './wallet-transactions/wallet-transactions.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { QRCodeModule } from 'angular2-qrcode';
+import { QrScannerModule } from 'angular2-qrscanner/dist';
+import { TransactionComponent } from './transaction/transaction.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { ShopProductComponent } from './shop-product/shop-product.component';
+import { ClipboardModule } from 'ngx-clipboard';
 
 @NgModule({
   declarations: [
@@ -48,7 +50,9 @@ import { ShopProductComponent } from './shop-product/shop-product.component';
     QrScannerModule,
     ReactiveFormsModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    ClipboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shop-product/shop-product.component.ts
+++ src/app/shop-product/shop-product.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Product} from '../product';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-shop-product',
@@ -8,10 +9,61 @@ import {Product} from '../product';
 })
 export class ShopProductComponent implements OnInit {
   @Input() product: Product;
+  showModal: boolean;
+  address: string;
 
-  constructor() { }
+  timeLeft: number;
+  countDown: Observable<any>;
+
+  orderPayed: boolean = false;
+  orderCanceled: boolean = false;
+  
+  constructor() {
+    this.showModal = false;
+    this.address = '0x32Be343B94f860124dC4fEe278FDCBD38C102D88';
+  }
 
   ngOnInit() {
   }
 
+  resetOrder() {
+    this.timeLeft = 60 * 10 - 590;
+    this.orderPayed = false;
+    this.orderCanceled = false;
+  }
+
+  orderProduct(product: Product) {
+    this.showModal = true;
+    this.resetOrder();
+    this.countDown = Observable.timer(150, 1000)
+      .take(this.timeLeft)
+      .takeWhile(() => this.timeLeft !== 0 || this.orderPayed || this.orderCanceled)
+      .do(() => { }
+      , () => { }
+      , () => {
+        if (this.orderPayed) {
+          this.showOrderPayed();
+        } else {
+          this.cancelOrder();
+        }
+      })
+      .map(() => {
+        --this.timeLeft
+        const amountOfMinutesLeft = Math.floor((this.timeLeft / 60) % 60).toString();
+        const amountOfSecondsLeft = (this.timeLeft % 60).toString();
+        return `${amountOfMinutesLeft.length === 1 ? `0${amountOfMinutesLeft}` : amountOfMinutesLeft}:${amountOfSecondsLeft.length === 1 ? `0${amountOfSecondsLeft}` : amountOfSecondsLeft}`;
+      });
+  }
+
+  showOrderPayed() {
+    throw new Error("Method not implemented.");
+  }
+
+  cancelOrder() {
+    this.orderCanceled = true;
+  }
+
+  hideModal() {
+    this.showModal = false;
+  }
 }
