46622a131c43c62c7943fba51a38282434c9280a
Luud van Keulen <l.vankeulen@student.fontys.nl>
2017-11-29 16:06:53 +0100

    Kleine TSLint error fix.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {AccountService} from './account.service';
-import {Router} from "@angular/router";
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
