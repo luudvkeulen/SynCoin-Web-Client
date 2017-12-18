88172f7114cc85f3e198ecda72af91c7bbc4411d
Bram de Boer <bramdeboer@outlook.com>
2017-12-18 11:42:00 +0100

    receive done

--- src/app/wallet-receive/wallet-receive.component.ts
+++ src/app/wallet-receive/wallet-receive.component.ts
@@ -1,17 +1,29 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { WalletService } from '../wallet.service';
 
 @Component({
   selector: 'app-wallet-receive',
   templateUrl: './wallet-receive.component.html',
   styleUrls: ['./wallet-receive.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  providers: [WalletService]
 })
 export class WalletReceiveComponent implements OnInit {
 
-  constructor() {
+  address: String;
+
+  constructor(private walletService: WalletService) {
   }
 
   ngOnInit() {
+    this.getBalance();
   }
 
+  getBalance() {
+    this.walletService.getWalletAddress().subscribe(result => {
+      this.address = result.address;
+    }, err => {
+      console.log(err);
+    });
+  }
 }
