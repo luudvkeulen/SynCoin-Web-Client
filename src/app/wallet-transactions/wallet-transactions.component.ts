6d2de143a7f80028af0981977a28a493295a8fdc
Maxime <maximedelange1991@hotmail.com>
2018-01-09 11:57:47 +0100

    General layout fixes for responsive layout for desktop and mobile. Added new layout fixes for tables and for transaction table

--- src/app/wallet-transactions/wallet-transactions.component.ts
+++ src/app/wallet-transactions/wallet-transactions.component.ts
@@ -7,10 +7,36 @@ import {Component, OnInit} from '@angular/core';
 })
 export class WalletTransactionsComponent implements OnInit {
 
+  isCollapsed: boolean;
+
   constructor() {
   }
 
   ngOnInit() {
+    this.showContent();
   }
 
+  showContent() {
+    if (!this.isCollapsed) {
+      const tdElements = document.getElementById('tableInfo').getElementsByTagName('td');
+      console.log('collapsed');
+      for (let i = 0; i < tdElements.length; i++) {
+        tdElements[i].style.overflow = 'hidden';
+        tdElements[i].style.whiteSpace = 'nowrap';
+        tdElements[i].style.textOverflow = 'ellipsis';
+      }
+      this.isCollapsed = true;
+    }else if (this.isCollapsed) {
+      const tdElements = document.getElementById('tableInfo').getElementsByTagName('td');
+      console.log('expanded');
+      for (let i = 0; i < tdElements.length; i++) {
+        tdElements[i].style.whiteSpace = '';
+        tdElements[i].style.maxWidth = '100px';
+        tdElements[i].style.overflow = 'auto';
+        tdElements[i].style.textOverflow = '';
+        tdElements[i].style.wordWrap = 'break-word';
+      }
+      this.isCollapsed = false;
+    }
+  }
 }
