c66412b85651af865473ec7376451296b827e617
Maxime <maximedelange1991@hotmail.com>
2018-01-09 14:42:33 +0100

    Merge branches 'master' and 'syntouchstyling' of https://github.com/luudvkeulen/SynCoin-Web-Client into syntouchstyling
    # Conflicts:
    #	src/app/wallet-transactions/wallet-transactions.component.html
    #	src/app/wallet-transactions/wallet-transactions.component.ts
    
    Laatste updates aan layout

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -9,6 +9,7 @@ export class OrdersComponent implements OnInit, OnChanges {
   @Input() orders;
   _orders;
   loading = true;
+  isCollapsed: boolean;
 
   constructor() {
   }
@@ -21,6 +22,31 @@ export class OrdersComponent implements OnInit, OnChanges {
     if (orders.currentValue != null) {
       this._orders = orders.currentValue;
       this.loading = false;
+    }
+  }
+
+  showContent() {
+    const tdElements = document.getElementById('tableInfo').getElementsByTagName('td');
+    if (tdElements != null) {
+      if (!this.isCollapsed) {
+        console.log('collapsed');
+        for (let i = 0; i < tdElements.length; i++) {
+          tdElements[i].style.overflow = 'hidden';
+          tdElements[i].style.whiteSpace = 'nowrap';
+          tdElements[i].style.textOverflow = 'ellipsis';
+        }
+        this.isCollapsed = true;
+      }else if (this.isCollapsed) {
+        console.log('expanded');
+        for (let i = 0; i < tdElements.length; i++) {
+          tdElements[i].style.whiteSpace = '';
+          tdElements[i].style.maxWidth = '100px';
+          tdElements[i].style.overflow = 'auto';
+          tdElements[i].style.textOverflow = '';
+          tdElements[i].style.wordWrap = 'break-word';
+        }
+        this.isCollapsed = false;
+      }
     }
   }
 }
