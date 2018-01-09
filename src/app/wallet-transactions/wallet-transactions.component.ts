c68fcb2529f0b779037d8d42cc8f2e32fe80f6cc
Luud van Keulen <luudvkeulen@gmail.com>
2018-01-09 13:55:09 +0100

    Transaction log

--- src/app/wallet-transactions/wallet-transactions.component.ts
+++ src/app/wallet-transactions/wallet-transactions.component.ts
@@ -1,16 +1,34 @@
 import {Component, OnInit} from '@angular/core';
+import {WalletService} from '../wallet.service';
 
 @Component({
   selector: 'app-wallet-transactions',
   templateUrl: './wallet-transactions.component.html',
-  styleUrls: ['./wallet-transactions.component.css']
+  styleUrls: ['./wallet-transactions.component.css'],
+  providers: [WalletService]
 })
 export class WalletTransactionsComponent implements OnInit {
+  transactions;
+  walletAddress;
 
-  constructor() {
+  constructor(private walletService: WalletService) {
   }
 
   ngOnInit() {
+    this.walletService.getUserTransactions().subscribe((result) => {
+      this.transactions = result;
+    }, err => {
+      console.log(err);
+    });
+
+    this.walletService.getWalletAddress().subscribe(result => {
+      this.walletAddress = result.address;
+    }, err => {
+      console.log(err);
+    });
   }
 
+  abs(value: number) {
+    return Math.abs(value);
+  }
 }
